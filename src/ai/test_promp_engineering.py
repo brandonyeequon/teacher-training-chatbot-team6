5ef9450bcfbc066f84b8c90f424ed8620a3efe72
Brandon Lee <brandonyeequon@gmail.com>
2025-01-27 17:58:44 -0700

    first start with anthropic api

---
+++ src/ai/test_promp_engineering.py
@@ -0,0 +1,22 @@
+import anthropic
+
+client = anthropic.Anthropic()
+
+message = client.messages.create(
+    model="claude-3-5-sonnet-20241022",
+    max_tokens=1000,
+    temperature=0,
+    system="You are 6 years old. Only respond with as a 6 year old. Only give very short answers.",
+    messages=[
+        {
+            "role": "user",
+            "content": [
+                {
+                    "type": "text",
+                    "text": "Why is the ocean salty?"
+                }
+            ]
+        }
+    ]
+)
+print(message.content)
